e7c626f84d13e0cdd84dbfb8d7643bf529ae7ed7
jinofuente <jinofuente.h@gmail.com>
2023-12-12 20:46:19 -0500

    simulando base de datos

---
+++ src/data/productos.js
@@ -0,0 +1,20 @@
+export default {
+    productos: [
+        {
+            id: 1,
+            nombre: 'tennis converse',
+            descripcion: 'lorem de arequiapa a puno',
+            precio: 500.0,
+            colores: ['negro', 'rojo', 'amarillo'],
+            tamaños: ['1.5', '2', '2.5', '3', '4']
+        },
+        {
+            id: 2,
+            nombre: 'tennissss converse',
+            descripcion: 'lorem de arequiapa a azangaro',
+            precio: 500.0,
+            colores: ['negro', 'rojo', 'amarillo'],
+            tamaños: ['1.5', '2', '2.5', '3', '4']
+        },
+    ]
+}
